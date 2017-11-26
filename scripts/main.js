06bb930857e949b65ffe67ae2ebaa3838dd824ab
daryaban <mousavi.itbusiness@gmail.com>
2017-11-26 15:54:24 +0330

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,40 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hellow world';
+
+function multiply(num1, num2) {
+    var result = num1 * num2;
+    return result;
+}
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function () {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc == 'images/firefox-logo-2017.png') {
+        myImage.setAttribute('src', 'images/firefox2.png');
+    } else {
+        myImage.setAttribute('src', 'images/firefox-logo-2017.png')
+    }
+
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function () {
+    setUserName();
+}
+
